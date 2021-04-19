f59ea44ea227b109fc902455744fc528768fefeb
Mina Bui <hello@minabui.com>
2021-04-18 21:40:56 -0700

    basic styling done, starting js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 // Scripts for the Love Calculator
+
+calculateLove() {
+    var nameOne = document.getElementById("name-one");
+    var nameTwo = document.getElementById("name-two");
+
+    if (nameOne == "") {
+        alert('Please enter your name');
+    } else if (nameOne.length <= 2) {
+        alert('Please enter a valid name.');
+    } else if (!isNaN(nameOne)) {
+        alert('Please enter valid characters only (Sorry, X Æ A-12).')
+    }
+
+    if (nameTwo == "") {
+        alert('Please enter your name');
+    } else if (nameTwo.length <= 2) {
+        alert('Please enter a valid name.');
+    } else if (!isNaN(nameTwo)) {
+        alert('Please enter valid characters only (Sorry, X Æ A-12).')
+    }
+
+}
